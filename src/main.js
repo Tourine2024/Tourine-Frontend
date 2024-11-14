104a19f3caaac8840ebabe79c4cf2928c9bd8b6c
이효승 <julia_2000@naver.com>
2024-11-14 16:29:47 +0900

    feat:  vue 초기세팅

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./assets/style/main.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import App from "./App.vue";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
