a4faa058592821c1a3c59792d1659b3b4616a137
pww1101 <pww1101@gmail.com>
2024-11-17 01:03:49 +0900

    feat: 의존성 추가

--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
 import "./assets/style/main.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 import App from "./App.vue";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
 import router from "./router";
+
+// Vuetify
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.use(vuetify).mount("#app");
