46165561ac53b83be6ee647c2253bd6b1dc4667f
pww1101 <pww1101@gmail.com>
2024-11-17 01:16:06 +0900

    feat: 여행 상세 페이지("/trip/:tripNo") 추가

---
+++ src/components/MapItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <GoogleMap :api-key="GOOGLE_MAPS_API_KEY" style="width: 100%; height: 500px" :center="center" :zoom="15" class="px-4 py-5" />
+    <Marker :options="{ position: center }" />
+  </div>
+</template>
+
+<script setup>
+import { GoogleMap, Marker } from "vue3-google-map";
+
+const center = { lat: 15.882632, lng: 108.325234 };
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+</script>
+
+<style scoped></style>
---
+++ src/components/TripDetailTitleItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-row align="start">
+      <v-col cols="6">
+        <v-card variant="text">
+          <v-card-subtitle>{{ trip.tripStartDate }} - {{ trip.tripEndDate }}</v-card-subtitle>
+          <h1 class="font-weight-black ml-4">{{ trip.tripName }}</h1>
+          <v-card-text>
+            AI 여행 요약<br />
+            {{ trip.tripSummary }}
+          </v-card-text>
+          <v-btn class="md-3 ml-4" rounded="xl" color="blue">AI 요약하기+</v-btn>
+          <v-btn class="md-3 ml-4" rounded="xl" color="blue">수정하기</v-btn>
+        </v-card>
+      </v-col>
+      <v-col class="text-xs-center">
+        <!-- :src="trip.tripThumbnailUrl" -->
+        <v-img src="https://www.casenews.co.kr/news/photo/202408/16250_35442_5945.jpg" class="md-2 rounded-circle" width="250px" height="250px" cover />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  trip: {
+    tripNo: Number,
+    tripName: String,
+    tripSummary: String,
+    tripThumbnailUrl: String,
+    tripStartDate: String,
+    tripEndDate: String,
+    memberNo: Number,
+  },
+});
+</script>
+
+<style scoped></style>
