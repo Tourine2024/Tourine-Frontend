fbc9fc4a62c5a21124fb68c48b227b9ad1002fcd
pww1101 <pww1101@gmail.com>
2024-11-18 02:27:34 +0900

    feat: route 추가("/trips/new": 여행 생성, "/trip/:tripNo/diary/new": 여행 기록 생성, "/trip/:tripNo/diary/:diaryNo": 여행 기록 상세)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AboutView from "../views/AboutView.vue";
+import DiaryDetailView from "@/views/DiaryDetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,17 +14,32 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("@/views/AboutView.vue"),
     },
     {
       path: "/trips",
       name: "trips",
-      component: () => import("../views/TripsView.vue"),
+      component: () => import("@/views/TripsView.vue"),
+    },
+    {
+      path: "/trips/new",
+      name: "tripNew",
+      component: () => import("@/views/TripInsertView.vue"),
     },
     {
       path: "/trip/:tripNo",
       name: "tripDetail",
-      component: () => import("../views/TripDetailView.vue"),
+      component: () => import("@/views/TripDetailView.vue"),
+    },
+    {
+      path: "/trip/:tripNo/diary/new",
+      name: "diaryNew",
+      component: () => import("@/views/DiaryInsertView.vue"),
+    },
+    {
+      path: "/trip/:tripNo/diary/:diaryNo",
+      name: "diaryDetail",
+      component: () => import("@/views/DiaryDetailView.vue"),
     },
   ],
 });
