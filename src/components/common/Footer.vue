bb839cf22ae1b64e5894c9f64a093b85576f0e41
이효승 <hyoseung.lee2000@gmail.com>
2024-11-18 17:38:36 +0900

    bug : footer 하단 고정 수정

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-footer class="d-flex flex-column"
-    >@2024 Tourine. All Right Reserved.</v-footer
-  >
+  <v-footer class="d-flex flex-column">@2024 Tourine. All Right Reserved.</v-footer>
 </template>
 
 <style scoped>
 .v-footer {
+  position: absolute;
+  bottom: 0;
   background-color: #00507a;
   color: white;
   padding: 10px;
   text-align: center;
+  max-height: 90px;
+  width: 100%;
 }
 </style>
